4a5f0f591b3b394d034ff0399ae0307126e378b8
Ian Stewart <ianstewart4outlook.com>
2021-06-11 14:01:26 -0500

    added playRound function

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 // Rock Paper Scissors Game
 // Make computerPlay function that will automatically return rock/paper/scissors
 function computerPlay() {
+    // randomly select rock/paper/scissors
+    let pcPlay = ['Rock', 'Paper', 'Scissors'];
+    let randomPlay = pcPlay[Math.floor(Math.random() * 3)];
+    console.log(randomPlay);
+    return randomPlay;
+}
+// Get user input
+
+// Change to lowercase
+// Check if input is correct
+// Compare to pcplay, determine winner
+function playRound(playerSelection, computerSelection){
+    if (playerSelection.toLowerCase() == "rock"){
+        console.log('You chose ROCK')
+    } else if (playerSelection.toLowerCase() == "paper"){
+        console.log('You chose PAPER')
+    } else if (playerSelection.toLowerCase() == "scissors"){
+        console.log('You chose SCISSORS')
+    } else {
+        console.log('Player did not enter a valid selection')
+    }
     
 }
