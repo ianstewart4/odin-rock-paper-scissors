7a207d57e3bd012d9952e3c36df48d9ffe170434
Ian Stewart <ianstewart4outlook.com>
2021-06-11 21:27:32 -0500

    added more functions, computerPlay and userPlay is almost done, just needs minor work

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ function computerPlay() {
     // randomly select rock/paper/scissors
     let pcPlay = ['Rock', 'Paper', 'Scissors'];
     let randomPlay = pcPlay[Math.floor(Math.random() * 3)];
-    console.log(randomPlay);
-    return randomPlay;
+    // console.log(randomPlay);
+    return randomPlay.toLowerCase();
 }
 // Get user input
+function getInput() {
+    return prompt('Rock, Paper, Scissors?');
+}
 
-// Change to lowercase
 // Check if input is correct
 // Compare to pcplay, determine winner
 function playRound(playerSelection, computerSelection){
     if (playerSelection.toLowerCase() == "rock"){
-        console.log('You chose ROCK')
+        console.log('You chose ROCK');
+        if (computerSelection = '')
     } else if (playerSelection.toLowerCase() == "paper"){
-        console.log('You chose PAPER')
+        console.log('You chose PAPER');
     } else if (playerSelection.toLowerCase() == "scissors"){
-        console.log('You chose SCISSORS')
+        console.log('You chose SCISSORS');
     } else {
-        console.log('Player did not enter a valid selection')
+        console.log('Player did not enter a valid selection');
     }
     
+}
+let gameOn = true;
+
+while (gameOn) {
+
 }
