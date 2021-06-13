ff88a8bddb41c1d6f940ea5c0c4479f7959507c6
Ian Stewart <ianstewart4outlook.com>
2021-06-13 08:25:02 -0500

    made function to play single game

--- script.js
+++ script.js
@@ -4,24 +4,47 @@ function computerPlay() {
     // randomly select rock/paper/scissors
     let pcPlay = ['Rock', 'Paper', 'Scissors'];
     let randomPlay = pcPlay[Math.floor(Math.random() * 3)];
-    // console.log(randomPlay);
     return randomPlay.toLowerCase();
 }
 // Get user input
 function getInput() {
-    return prompt('Rock, Paper, Scissors?');
+    let userPlay = prompt('Rock, Paper, Scissors?').toLowerCase();
+    return userPlay;
 }
 
 // Check if input is correct
-// Compare to pcplay, determine winner
+// Compare to computerPlay, determine winner
 function playRound(playerSelection, computerSelection){
-    if (playerSelection.toLowerCase() == "rock"){
+    console.log('Computer selected: ' + computerSelection);
+    if (playerSelection == "rock"){
         console.log('You chose ROCK');
-        if (computerSelection = '')
-    } else if (playerSelection.toLowerCase() == "paper"){
+        if (computerSelection == 'rock'){
+            console.log('TIE!');
+        } else if (computerSelection == 'paper'){
+            console.log('Paper beats rock, you lose!');
+        } else if (computerSelection == 'scissors'){
+            console.log('Rock beats scissors, you win!');
+        }
+        
+    } else if (playerSelection == "paper"){
         console.log('You chose PAPER');
-    } else if (playerSelection.toLowerCase() == "scissors"){
+        if (computerSelection == 'paper'){
+            console.log('TIE!');
+        } else if (computerSelection == 'rock'){
+            console.log('Paper beats rock, you win!');
+        } else if (computerSelection == 'scissors'){
+            console.log('Scissors beat paper, you lose!');
+        }
+
+    } else if (playerSelection == "scissors"){
         console.log('You chose SCISSORS');
+        if (computerSelection == 'scissors'){
+            console.log('TIE!');
+        } else if (computerSelection == 'paper'){
+            console.log('Scissors beat paper, you win!');
+        } else if (computerSelection == 'rock'){
+            console.log('Rock beats scissors, you lose!');
+        }
     } else {
         console.log('Player did not enter a valid selection');
     }
@@ -29,6 +52,8 @@ function playRound(playerSelection, computerSelection){
 }
 let gameOn = true;
 
-while (gameOn) {
+playRound(getInput(),computerPlay());
 
-}
+// while (gameOn) {
+//     playRound(getInput(),computerPlay());
+// }
